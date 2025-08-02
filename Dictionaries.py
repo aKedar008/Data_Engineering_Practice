ba3b39ff896e3855420c598fd23969b4c881294f
aKedar008 <akedar474@gmail.com>
2025-08-02 23:13:42 +0530

    Updated Dictionaries.py file with more practice examples

--- Dictionaries.py
+++ Dictionaries.py
@@ -36,3 +36,53 @@ print(f"New position: {alien_0['x_position']}")
 alien_0 = {'color': 'green', 'points':5}
 del alien_0['points']
 print(alien_0)
+
+# You can also use a dictionary to store one kind of information about many objects
+
+favorite_language = {
+    'Jen' : 'Pyhton',
+    'Sarah' : 'C',
+    'Edward': 'Ruby',
+    'Phil': 'Pyhton'
+}
+language = favorite_language['Sarah'].title()
+print(f"Sarah's favorite language is {language}")
+
+# Using get() to Access Values
+
+point_value = alien_0.get('points','No point value assigned')
+print(point_value)
+
+# TIY 6.1 Use a dictionary to store information about a person you know.
+# Store their first name, last name, age, and the city in which they live. You
+# should have keys such as first_name, last_name, age, and city. Print each
+# piece of information stored in your dictionary.
+
+person = {
+    'f_name': 'Shlok',
+    'l_name': 'Kedar',
+    'Age': 19,
+    'City': 'Nagpur',    
+}
+
+print(f"First Name: {person['f_name']}")
+print(f"Last Name: {person['l_name']}")
+print(f"Age: {person['Age']}")
+print(f"City: {person['City']}")
+
+# Looping through the Dictionary 
+# Looping Through All Key-Value Pairs
+
+user_0 = {
+ 'username': 'efermi',
+ 'first': 'enrico',
+ 'last': 'fermi',
+ }
+
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value:{value}")
+
+for name, language in favorite_language.items():
+    print(f"{name.title()}'s favurite language is {language.title()}.")
+
