103783934a07ed97d9621f687c815092215d8575
aKedar008 <akedar474@gmail.com>
2025-08-18 22:57:03 +0530

    Learning Functions

---
+++ functions.py
@@ -0,0 +1,33 @@
+# Functions = functions, which are named blocks of code
+# that are designed to do one specific job.
+# When you want to perform a particular task
+# that you’ve defined in a function, you call the function
+# responsible for it.
+
+def greet_user():
+    """Display a simple greeting"""
+    print("Hello User")
+
+greet_user()
+
+# passing information to a function 
+
+def greet_user(username):
+    """Display a simple greeting"""
+    print(f"Hello, {username.title()}!")
+
+greet_user('akedar008')
+
+# An argument is a piece of information that’s passed from a function call to a function.
+# In this case the argument 'akedar008' was passed to the function greet_user(), and the value was assigned to the parameter username.
+
+def display_message():
+    print("Learning Python")
+display_message()
+
+def favourite_book(name):
+    """Enter Your Favourite book name"""
+    print(f"My favorite book is, {name.title()}")
+
+favourite_book('Atomic habits')
+
